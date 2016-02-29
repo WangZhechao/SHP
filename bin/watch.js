8c8da0061f68c620dde74970b38caeedbc4a8b5f
WangZhechao <wangzhechao@live.com>
2016-02-29 23:13:00 +0800

    修正watch文件Promise的写法，继续完善监视功能。

--- bin/watch.js
+++ bin/watch.js
@@ -1,67 +1,78 @@
 var Promise = require('bluebird'),
+	fs = Promise.promisifyAll(require("fs")),
 	path = require('path'),
-	fs = require('fs'),
+	isWindow = (process.platform === 'win32'),
 	config = require(path.resolve(__dirname, '../lib/config')),
-	errors = require(path.resolve(__dirname, '../lib/errors')),
-	nodeVersion = process.version.split("."),
-	isWindowsWithoutWatchFile = process.platform === 'win32' && parseInt(nodeVersion[1]) <= 6;
+	errors = require(path.resolve(__dirname, '../lib/errors'));
+	
+
+function crash() {
+	console.log('====================');
+}
 
 
-function crashWin(event, fileName) {
-
+function crashWin(event, fileName) {console.log(fileName);
+	if(event === 'change') {
+		crash();
+	}
 }
 
 function crashOther(oldStat, newStat) {
-
+	if(oldStat.mtime.getTime() !== newStat.mtime.getTime()) {
+		crash();
+	}
 }
 
 function watchFile(watch, poll_interval) {
-	if(isWindowsWithoutWatchFile) {
+	if(isWindow) {
 		return fs.watch(watch, { persistent: true, interval: poll_interval }, crashWin);
 	} else {
 		return fs.watchFile(watch, { persistent: true, interval: poll_interval }, crashOther);
 	}
 }
 
-function findAllWatchFiles(dir) {
+function findAllWatchFiles(dir, watchFiles) {
 	var extensions = 'node,js',
 		fileExtensionPattern = new RegExp('^.*\.(' + extensions.replace(/,/g, '|') + ')$');
 
 	dir = path.resolve(dir);
 
-	fs.stat(dir, function(err, stats) {
-		if(err) {
-			return errors.logAndRejectError(err, '获取目录信息失败：' + dir, '请查看该目录是否存在！');
-		}
-		
+	return fs.statAsync(dir).then(function(stats) {
 		if(stats.isDirectory()) {
-			if(isWindowsWithoutWatchFile) {
-				return Promise.resolve(dir);
-			}
-
-			fs.readdir(dir, function(err, fileNames) {
-				if(err) {
-					return errors.logAndRejectError(err, '读取文件夹信息失败！', '请检测文件夹是否存在！');
-				}
-
+			return fs.readdirAsync(dir).then(function(fileNames) {
+				var files = [];
+				
 				fileNames.forEach(function(fileName) {
-					return findAllWatchFiles(path.join(dir, fileName));
+					files.push(findAllWatchFiles(path.join(dir, fileName), watchFiles));
 				});
+
+				return Promise.all(files);
+			}).catch(function(err) {
+				return errors.logAndRejectError(err, '读取文件夹信息失败！', '请检测文件夹是否存在！');
 			});
 		} else if(stats.isFile() && dir.match(fileExtensionPattern)){
+			watchFiles.push(dir);
 			return Promise.resolve(dir);
 		}
+	}).catch(function(err) {
+		return errors.logAndRejectError(err, '获取目录信息失败：' + dir, '请查看该目录是否存在！');
 	});
 }
 
 
 function run(args) {
 	var pollInterval = 1000,
-		path = config.paths.appPath;
+		path = config.paths.appPath,
+		watchFiles = [];
 
-	findAllWatchFiles(path, function(file) {
-		watchFile(file, pollInterval);
-	});
+
+	findAllWatchFiles(path, watchFiles).then(function() {
+		return Promise.each(watchFiles, function(item) {
+			watchFile(item, pollInterval);
+		});
+	});	
+
+	require('../index.js');
 }
 
 
