1038a802185b4d5a098fdffb03d6425b96b4443d
WangZhechao <wangzhechao@live.com>
2016-03-01 22:14:22 +0800

    增加watch.js文件，完成文件改变监视功能，方便调试。

--- bin/watch.js
+++ bin/watch.js
@@ -1,17 +1,67 @@
 var Promise = require('bluebird'),
 	fs = Promise.promisifyAll(require("fs")),
 	path = require('path'),
+	spawn = require("child_process").spawn,
 	isWindow = (process.platform === 'win32'),
 	config = require(path.resolve(__dirname, '../lib/config')),
-	errors = require(path.resolve(__dirname, '../lib/errors'));
+	errors = require(path.resolve(__dirname, '../lib/errors')),
+	restarting = false,
+	childProcess,
+	exec = 'node',
+	prog = [path.join(config.paths.shpPath, 'index.js')];
 	
 
-function crash() {
-	console.log('====================');
+function startProgram() {
+	restarting = false;
+
+	childProcess = spawn(exec, prog, {stdio: 'inherit'});
+	childProcess.addListener('exit', function(code) {
+		//子进程启动失败，设置为null，下次crash自动重启。
+		if(!restarting) {
+			childProcess = null;
+			return;
+		}
+
+		startProgram();
+	});
+
+	try {
+	  // Pass kill signals through to child
+	  [ "SIGTERM", "SIGINT", "SIGHUP", "SIGQUIT" ].forEach( function(signal) {
+	    process.on(signal, function () {
+	      if (childProcess) {
+	        childProcess.kill(signal);
+	      }
+
+	      process.exit();
+	    });
+	  });
+	} catch(e) {
+	  // Windows doesn't support signals yet, so they simply don't get this handling.
+	  // https://github.com/joyent/node/issues/1553
+	}
+
+	console.log('主进程：' + process.pid, '子进程：' + childProcess.pid);
 }
 
 
-function crashWin(event, fileName) {console.log(fileName);
+function crash() {
+	if(restarting) {
+		return;
+	}
+
+	restarting = true;
+	setTimeout(function() {
+		if(childProcess) {
+			process.kill(childProcess.pid);
+		} else {
+			startProgram();
+		}
+	}, 50);
+}
+
+
+function crashWin(event, fileName) {
 	if(event === 'change') {
 		crash();
 	}
@@ -31,49 +81,54 @@ function watchFile(watch, poll_interval) {
 	}
 }
 
-function findAllWatchFiles(dir, watchFiles) {
-	var extensions = 'node,js',
+
+function findAllWatchFiles(dir) {
+	var watchFiles = [],
+		extensions = 'node,js',
 		fileExtensionPattern = new RegExp('^.*\.(' + extensions.replace(/,/g, '|') + ')$');
 
 	dir = path.resolve(dir);
 
-	return fs.statAsync(dir).then(function(stats) {
-		if(stats.isDirectory()) {
-			return fs.readdirAsync(dir).then(function(fileNames) {
-				var files = [];
-				
-				fileNames.forEach(function(fileName) {
-					files.push(findAllWatchFiles(path.join(dir, fileName), watchFiles));
-				});
+	function findFiles(dir) {
+		return fs.statAsync(dir).then(function(stats) {
+			if(stats.isDirectory()) {
+				return fs.readdirAsync(dir).then(function(fileNames) {
+					var files = [];
+					
+					fileNames.forEach(function(fileName) {
+						files.push(findFiles(path.join(dir, fileName), watchFiles));
+					});
 
-				return Promise.all(files);
-			}).catch(function(err) {
-				return errors.logAndRejectError(err, '读取文件夹信息失败！', '请检测文件夹是否存在！');
-			});
-		} else if(stats.isFile() && dir.match(fileExtensionPattern)){
-			watchFiles.push(dir);
-			return Promise.resolve(dir);
-		}
-	}).catch(function(err) {
-		return errors.logAndRejectError(err, '获取目录信息失败：' + dir, '请查看该目录是否存在！');
+					return Promise.all(files);
+				}).catch(function(err) {
+					return errors.logAndRejectError(err, '读取文件夹信息失败！', '请检测文件夹是否存在！');
+				});
+			} else if(stats.isFile() && dir.match(fileExtensionPattern)){
+				watchFiles.push(dir);
+				return Promise.resolve(dir);
+			}
+		}).catch(function(err) {
+			return errors.logAndRejectError(err, '获取目录信息失败：' + dir, '请查看该目录是否存在！');
+		});
+	}
+
+	return findFiles(dir).then(function() {
+		return watchFiles;
 	});
 }
 
 
 function run(args) {
 	var pollInterval = 1000,
-		path = config.paths.appPath,
-		watchFiles = [];
+		path = config.paths.appPath;
 
-
-	findAllWatchFiles(path, watchFiles).then(function() {
-		return Promise.each(watchFiles, function(item) {
+	findAllWatchFiles(path).then(function(files) {
+		return Promise.each(files, function(item) {
 			watchFile(item, pollInterval);
 		});
 	});	
 
-	require('../index.js');
+	startProgram();
 }
-
 
 run();
