10e2d876d8ad14a15f5b274e03d4c68638c6c221
WangZhechao <wangzhechao@live.com>
2016-03-28 22:33:16 +0800

    继续完善gulpfile文件。

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,50 @@
 var gulp = require('gulp');
 var mocha = require('gulp-mocha');
+var jshint = require('gulp-jshint');
+var uglify = require('gulp-uglify');
+var merge = require('merge-stream');
+var obfuscate = require('gulp-obfuscate');
 
 gulp.task('default', function() {
-	return gulp.src(['test/**'], { read: false, nodir: true })
-	  .pipe(mocha({
-	    reporter: 'spec'
-	  }));
+
 });
 
+
+gulp.task('test', function() {
+	return gulp.src(['test/**'], { read: false, nodir: true })
+		.pipe(mocha({
+			reporter: 'spec'
+		}));
+});
+
+gulp.task('lint', function() {
+	return gulp.src(['./application/**/*.js', './lib/**/*.js'])
+		.pipe(jshint())
+		.pipe(jshint.reporter('default', { verbose: true }));
+});
+
+
+gulp.task('compress', function() {
+	var folders = [
+			['./application/**/*.js', './dist/application'], 
+			['./lib/**/*.js', './dist/lib'],
+			['./index.js', './dist'],
+			['./config.js', './dist']
+		];
+
+	var tasks = folders.map(function(folder) {
+		return gulp.src(folder[0])
+			.pipe(uglify())
+			.pipe(gulp.dest(folder[1]));
+	});
+
+	return merge(tasks);
+});
+
+gulp.task('obfuscate', function() {
+	return gulp.src('./dist/**/*.js')
+	    .pipe(obfuscate({ replaceMethod: obfuscate.ZALGO }));
+});
 
 //测试
 
